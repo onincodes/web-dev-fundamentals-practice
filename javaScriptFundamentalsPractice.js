fd931d3bf1ef87a7a0f5321a040b2bb254ce8b7f
Onin Sibua <sibuanino@gmail.com>
2025-03-19 20:56:15 +0800

    Updated

--- javaScriptFundamentalsPractice.js
+++ javaScriptFundamentalsPractice.js
@@ -102,3 +102,28 @@ function longestWord(sentence) {
 // Test cases
 console.log(longestWord("The quick brown fox jumps over the lazy dog")); // Expected output: "jumps"
 console.log(longestWord("JavaScript is awesome")); // Expected output: "JavaScript"
+
+console.log(
+  `-----------------------------------Challenge #6---------------------------------------------`
+);
+
+// Challenge #6: FizzBuzz
+// Write a function that prints numbers from 1 to n. But for multiples of 3, print "Fizz" instead of the number,
+// and for multiples of 5, print "Buzz". For numbers that are multiples of both 3 and 5, print "FizzBuzz".
+
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// Test case
+fizzBuzz(15);
